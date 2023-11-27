ffe92d3498375cadb190e23eebcfa3210b60e6e7
emreyilmaz1912 <emreyilmaz264@gmail.com>
2023-11-27 23:18:21 +0300

    third commit

--- src/main/java/com/emreyilmaz/upodhotelreservationsystem/HelloApplication.java
+++ src/main/java/com/emreyilmaz/upodhotelreservationsystem/HelloApplication.java
@@ -11,23 +11,20 @@ import javafx.scene.image.Image;
 import javafx.stage.Stage;
 
 import java.io.IOException;
-import java.net.URL;
-import java.net.URLClassLoader;
-import java.nio.file.FileSystems;
-import java.nio.file.Path;
-import java.nio.file.Paths;
+
+
+import static com.emreyilmaz.upodhotelreservationsystem.Utils.LOGO;
+
 
 
 public class HelloApplication extends Application {
-
+/*
     private ObservableList<Room> roomsList = FXCollections.observableArrayList();
     private ObservableList<Customers> customersList = FXCollections.observableArrayList();
     private ObservableList<Reservations> reservationsList = FXCollections.observableArrayList();
     private ObservableList<Features> featuresList = FXCollections.observableArrayList();
     private ObservableList<Services> servicesList = FXCollections.observableArrayList();
-    Path rootPaths = Paths.get(".").normalize().toAbsolutePath();;
-    Path filePath = Paths.get(rootPaths.toString(),"src", "main", "resources", "com", "emreyilmaz", "upodhotelreservationsystem","img","icon_upod.png");
-    Image icon = new Image(filePath.toUri().toString());
+*/
     @Override
     public void start(Stage stage) throws IOException {
         FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("main-login.fxml"));
@@ -35,7 +32,7 @@ public class HelloApplication extends Application {
         Scene scene = new Scene(root);
         stage.setTitle("Upod Hotel & Resort Reservation Manager System");
 
-
+        Image icon = new Image(LOGO.toUri().toString());
         stage.getIcons().add(icon);
         stage.setScene(scene);
 
---
+++ src/main/java/com/emreyilmaz/upodhotelreservationsystem/Utils.java
@@ -0,0 +1,13 @@
+package com.emreyilmaz.upodhotelreservationsystem;
+
+import javafx.scene.image.Image;
+
+import java.nio.file.Path;
+import java.nio.file.Paths;
+
+public class Utils {
+
+    public static  final Path root = Paths.get(".").normalize().toAbsolutePath();;
+    public static  final Path LOGO = Paths.get(root.toString(),"src", "main", "resources", "com", "emreyilmaz", "upodhotelreservationsystem","img","icon_upod.png");
+
+}
